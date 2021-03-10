1b9fdfd46096e818ace62e6a646543293cb4fa7f
Nikolay98 <nikolyay98@mail.ru>
2021-03-10 21:44:05 +0300

    Initial commit

---
+++ src/company/Main.java
@@ -0,0 +1,21 @@
+package company;
+
+public class Main {
+    public static void mai(String [] args){
+        Cat[] cats = new Cat[3];
+        cats[0] = new Cat("Мурка", 5);
+        cats[1] = new Cat("Журка", 6);
+        cats[2] = new Cat("Дуська", 10);
+        Plate plate = new Plate(20);
+        for (int cat = 0; cat < cats.length; cat++){
+            cats[cat].eat(plate);
+        }
+        appendFoodTo(plate, 50);
+        for (int cat = 0; cat < cats.length; cat++){
+            cats[cat].eat(plate);
+        }
+    }
+    static void appendFoodTo(Plate plate, int food){
+        plate.increaseFood(food);
+    }
+}
---
+++ src/company/Plate.java
@@ -0,0 +1,17 @@
+package company;
+
+public class Plate {
+    private int food;
+    public Plate(int foo){
+        this.food = food;
+    }
+    public void increaseFood(int food){
+        this.food += food;
+    }
+    public void decreaseFood(int appetite){
+        food = food - appetite;
+    }
+    public boolean hasEnoughFood(int appetite){
+        return appetite <= food;
+    }
+}
